05a5944f8c49dc6a5e8750e19860e7ac05fa9485
PauloCruz096 <92983557+PauloCruz096@users.noreply.github.com>
2021-10-24 10:52:43 -0300

    Questão 1

---
+++ Questao_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    int i=3, j=5;
+    int *p, *q;
+    p = &i;
+    q = &j;
+    p == &i; // Resultado: True
+    cout << endl;
+    cout << *p - *q << endl; // Resultado: -2
+    cout << **&p << endl; // Resultado: 3
+    cout << 3 - *p/(*q) + 7 << endl; // Resultado: 10
+
+}
